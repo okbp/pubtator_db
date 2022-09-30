c5c7ec342b95ad03f1a5b589f60e6c7292ec741f
okbp <yokookbio@gmail.com>
2022-09-30 18:14:25 +0900

    virtuoso query by app

--- ruby/virtuoso_query.rb
+++ ruby/virtuoso_query.rb
@@ -7,24 +7,27 @@ def query(query_text)
     }
   }
 end
-sparql = SPARQL::Client.new("http://localhost:28890/sparql")
-query = <<~SPARQL
+sparql_client = SPARQL::Client.new("http://localhost:28890/sparql")
+query_text = <<~SPARQL
   DEFINE sql:select-option "order"
-  
+
   prefix ex: <http://example.com/>
-  
+
   SELECT ?gene_id (count(DISTINCT ?pmid) AS ?count)
   FROM <https://pubmed.ncbi.nlm.nih.gov/pubtator>
   {
     ?pmid ex:has_species "9606" .
     ?pmid ex:has_gene ?gene_id .
   } GROUP BY ?gene_id ORDER BY DESC(?count)
-  ;
 SPARQL
 
 # クエリ実行と時間計測
 start_time = Time.now
-ret = query(query)
+ret = sparql_client.query(query_text, **{content_type: 'application/sparql-results+json', read_timeout: 3600, keep_alive: 3600}).map {|binding|
+  binding.each_with_object({}) {|(name, term), hash|
+    hash[name] = term.to_s
+  }
+}
 p ret.size
 p "query time: #{Time.now - start_time}s"
 
